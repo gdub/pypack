9f3fd6a9a8fe2584f94fa52022164c44b112b9d8
Gary Wilson Jr <gary@thegarywilson.com>
2011-06-21 00:11:06 -0500

    Made the default URL be empty string.

--- pypack/commands.py
+++ pypack/commands.py
@@ -11,9 +11,9 @@ def pypack_command():
     usage = "%prog [options] package_name"
     version = "pypack, version %s, %s" % (pypack.__version__, pypack.__url__)
     parser = OptionParser(usage=usage, version=version)
-    parser.add_option("-a", "--author", dest="author", default='',
+    parser.add_option("-a", "--author", dest="author", default="",
                       help="The author name to use for the created package.")
-    parser.add_option("-d", "--desc", dest="desc", default='',
+    parser.add_option("-d", "--desc", dest="desc", default="",
                       help="A short description for the created package.")
     parser.add_option("-e", "--email", dest="email", default='',
                       help=("The author e-mail address to use for the created"
@@ -21,13 +21,13 @@ def pypack_command():
     parser.add_option("--initial-version", dest="version", default="0.1",
                       help=("The initial version number to use for the created"
                             " package, e.g. 0.2, 1.0, 1.0-rc1, etc."))
-    parser.add_option("-t", "--target",  dest="target", default=None,
+    parser.add_option("-t", "--target", dest="target", default=None,
                       help=("Specify a target directory to place the package"
                             " skeleton files in (the directory must not"
                             " already exist).  If not given, then directory"
                             " will default to the given name of the package,"
                             " and will be placed in the current directory."))
-    parser.add_option("-u", "--url", dest="url", default="http://example.com",
+    parser.add_option("-u", "--url", dest="url", default="",
                       help=("The URL to use for the created package."))
 #    parser.add_option("-l", "--license",  dest="license",
 #                      default="placeholder",
