8a86143dcc53f0dcdaab4bfcdc578afbd2fa5b60
Gary Wilson Jr <gary@thegarywilson.com>
2011-06-20 23:53:43 -0500

    Added more docstrings, factored out the building of context to its own method, and made it so that the package contents listing can be overridden with a parameter passed to the PackageBuilder constructor.

--- pypack/__init__.py
+++ pypack/__init__.py
@@ -12,12 +12,18 @@ class PathExists(PypackError): pass
 
 
 class FSNode(object):
+    """
+    Base class for file system node types, e.g. File and Directory.
+    """
 
     def __init__(self, path):
         self.path = path
 
 
 class File(FSNode):
+    """
+    Represents a file who's content is based on a template.
+    """
 
     template_dir = os.path.join(__path__[0], 'templates')
 
@@ -42,8 +48,10 @@ class File(FSNode):
                 f.write(template.substitute(**context))
 
 
-
 class Directory(FSNode):
+    """
+    Represents a directory.
+    """
 
     def copy_to(self, dir, context):
         file_path = os.path.join(dir, self.path)
@@ -51,14 +59,36 @@ class Directory(FSNode):
 
 
 class PackageBuilder(object):
+    """
+    Defines package contents and how that content gets created.
+    """
 
-    def __init__(self, name, options):
+    def __init__(self, name, options, contents=None):
         self.name = self.check_name(name)
         if not options.target:
             options.target = os.path.join(os.getcwd(), name)
         self.target_dir = options.target
         self.pkg_dir = os.path.join(options.target, name)
-        self.context = {
+        self.options = options
+        if contents is None:
+            self.contents = [
+                File('CHANGES.txt', 'changes.txt'),
+                File('distribute_setup.py', substitutions=False),
+                File('LICENSE.txt', 'license.txt'),
+                File('MANIFEST.in', 'manifest.txt'),
+                File('README.txt', 'readme.txt'),
+                File('setup.py', 'setup.txt'),
+                Directory(self.name),
+                    File(os.path.join(self.name, '__init__.py'), 'empty.txt'),
+            ]
+        else:
+            self.contents = contents
+
+    def build_context(self, name, options):
+        """
+        Build and return a context dictionary, using passed name and options.
+        """
+        context = {
             'author': options.author,
             'author_email': options.email,
             'name': name,
@@ -70,20 +100,11 @@ class PackageBuilder(object):
 include *.txt
 recursive-include docs *.txt""",
         }
-        self.context['dashes'] = '=' * len(self.context['name'])
-        self.context['version_and_date'] = '%s - %s' % (self.context['version'],
-                                                        self.context['date'])
-        self.context['version_and_date_dashes'] = '=' * len(self.context['version_and_date'])
-        self.contents = [
-            File('CHANGES.txt', 'changes.txt'),
-            File('distribute_setup.py', substitutions=False),
-            File('LICENSE.txt', 'license.txt'),
-            File('MANIFEST.in', 'manifest.txt'),
-            File('README.txt', 'readme.txt'),
-            File('setup.py', 'setup.txt'),
-            Directory(self.name),
-                File(os.path.join(self.name, '__init__.py'), 'empty.txt'),
-        ]
+        context['dashes'] = '=' * len(context['name'])
+        context['version_and_date'] = '%s - %s' % (context['version'],
+                                                   context['date'])
+        context['version_and_date_dashes'] = '=' * len(context['version_and_date'])
+        return context
 
     @staticmethod
     def check_name(name):
@@ -96,10 +117,14 @@ recursive-include docs *.txt""",
         return name
 
     def make_package(self):
+        """
+        Create the package and its contents.
+        """
         if os.path.exists(self.target_dir):
             raise PathExists("Target directory exists; aborting.")
         os.mkdir(self.target_dir)
         os.mkdir(self.pkg_dir)
         for content in self.contents:
             print "creating file:", os.path.join(self.pkg_dir, content.path)
-            content.copy_to(self.pkg_dir, self.context)
+            content.copy_to(self.pkg_dir,
+                            self.build_context(self.name, self.options))
